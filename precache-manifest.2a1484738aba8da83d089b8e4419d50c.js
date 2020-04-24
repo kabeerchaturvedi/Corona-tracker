bf334b9c83b0b42d1d7ed0541ac1b4ce75075c5f
kabeerchaturvedi <kbrchaturvedi@gmail.com>
2020-04-24 23:53:07 +0530

    Updates

---
+++ precache-manifest.2a1484738aba8da83d089b8e4419d50c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8dd5dee4151e6a34e9136c27d0b32a",
+    "url": "/Corona-tracker/index.html"
+  },
+  {
+    "revision": "de28af304f4811d74c7e",
+    "url": "/Corona-tracker/static/css/main.b5bde9b4.chunk.css"
+  },
+  {
+    "revision": "c225adb75934ad1b638e",
+    "url": "/Corona-tracker/static/js/2.f4ae267f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-tracker/static/js/2.f4ae267f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de28af304f4811d74c7e",
+    "url": "/Corona-tracker/static/js/main.df32fabd.chunk.js"
+  },
+  {
+    "revision": "8a8f108aa7a87c625427",
+    "url": "/Corona-tracker/static/js/runtime-main.5f8ca858.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-tracker/static/media/image.d7265326.png"
+  }
+]);
